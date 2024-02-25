4ab351e18c5c46d693ee55825e8b150b35f40a81
Ivan Kapitanov <ivorybananaa@gmail.com>
2024-02-25 14:19:12 +0300

    feat: add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
+import * as path from "path";
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        path.resolve(__dirname, "../.env"),
+        path.resolve(__dirname, `../.app.${process.env.ENVIRONMENT}.env`),
+      ],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const isDev = configService.getOrThrow("ENVIRONMENT") === "dev";
+
+        return {
+          type: "mysql",
+          host: configService.getOrThrow("MYSQL_HOST"),
+          port: configService.getOrThrow("MYSQL_PORT"),
+          username: configService.getOrThrow("MYSQL_USER"),
+          password: configService.getOrThrow("MYSQL_PASSWORD"),
+          database: configService.getOrThrow("MYSQL_DATABASE"),
+          entities: [],
+          synchronize: isDev,
+          dropSchema: isDev,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
