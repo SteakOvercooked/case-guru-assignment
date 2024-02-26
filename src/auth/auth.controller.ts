aedd6f5523fbccff03c8306ac79131606e26afd7
Ivan Kapitanov <ivorybananaa@gmail.com>
2024-02-26 03:59:52 +0300

    fix: handle the case with login being non unique on sign up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   HttpCode,
@@ -11,10 +12,14 @@ import {
 import { AuthService } from "./auth.service";
 import { SignInDTO } from "./dto/signIn.dto";
 import { SignUpDTO } from "./dto/signUp.dto";
+import { UserService } from "../user/user.service";
 
 @Controller("auth")
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
 
   @UsePipes(new ValidationPipe())
   @HttpCode(HttpStatus.OK)
@@ -27,10 +32,16 @@ export class AuthController {
   @HttpCode(HttpStatus.CREATED)
   @Post("signup")
   async signUp(@Body() signUpDto: SignUpDTO) {
-    return this.authService.signUp(
-      signUpDto.login,
-      signUpDto.password,
-      signUpDto.fio,
-    );
+    try {
+      await this.userService.getByLogin(signUpDto.login);
+    } catch (e) {
+      return this.authService.signUp(
+        signUpDto.login,
+        signUpDto.password,
+        signUpDto.fio,
+      );
+    }
+
+    throw new BadRequestException("Login is already taken");
   }
 }
